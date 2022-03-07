efa320f04cda7ddae28cfd4ccb39c64d71585308
Graham Hoyes <accounts@grahamhoyes.com>
2022-03-07 15:45:02 -0500

    Symbols layer

--- keyboards/aleblazer/zodiark/keymaps/grahamhoyes/keymap.c
+++ keyboards/aleblazer/zodiark/keymaps/grahamhoyes/keymap.c
@@ -25,9 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layers {
     _BASE,
     _MAC,
-    _LOWER,
-    _RAISE,
     _NUMPAD,
+    _SYMBOL,
 };
 
 enum custom_keycodes {
@@ -92,11 +91,11 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        KC_AMPR , KC_LBRACKET , KC_LCBR   , KC_RCBR    , KC_LPRN           , KC_EQUAL ,                                                  KC_ASTR , KC_RPRN , KC_PLUS , KC_RBRACKET , KC_EXLM  , KC_GRAVE ,
-        KC_TAB  , KC_SCOLON   , KC_COMMA  , KC_DOT     , KC_P              , KC_Y     , KC_AT   ,                     KC_BSLS          , KC_F    , KC_G    , KC_C    , KC_R        , KC_L     , KC_SLASH ,
-        KC_ESC  , KC_A        , KC_O      , KC_E       , LT(_NUMPAD, KC_U) , KC_I     , KC_HOME ,                     KC_END           , KC_D    , KC_H    , KC_T    , KC_N        , KC_S     , KC_MINUS ,
-        KC_LSFT , KC_QUOTE    , KC_Q      , KC_J       , KC_K              , KC_X     , KC_LALT , KC_MUTE , RGB_TOG , TD(TD_SUPER_MAC) , KC_B    , KC_M    , KC_W    , KC_V        , KC_Z     , KC_RSFT  ,
-        KC_LCTL , KC_MEH      , KC_PC_CUT , KC_PC_COPY , KC_PC_PASTE       ,            KC_BSPC , KC_DEL  , KC_ENT  , KC_SPACE         ,           KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , KC_RCTRL
+        KC_AMPR , KC_LBRACKET , KC_LCBR   , KC_RCBR             , KC_LPRN           , KC_EQUAL           ,                                                  KC_ASTR , KC_RPRN , KC_PLUS , KC_RBRACKET , KC_EXLM  , KC_GRAVE ,
+        KC_TAB  , KC_SCOLON   , KC_COMMA  , LT(_SYMBOL, KC_DOT) , KC_P              , KC_Y               , KC_AT   ,                     KC_BSLS          , KC_F    , KC_G    , KC_C    , KC_R        , KC_L     , KC_SLASH ,
+        KC_ESC  , KC_A        , KC_O      , LT(_SYMBOL, KC_E)   , LT(_NUMPAD, KC_U) , LT(_SYMBOL, KC_I)  , KC_HOME ,                     KC_END           , KC_D    , KC_H    , KC_T    , KC_N        , KC_S     , KC_MINUS ,
+        KC_LSFT , KC_QUOTE    , KC_Q      , KC_J                , KC_K              , KC_X               , KC_LALT , KC_MUTE , RGB_TOG , TD(TD_SUPER_MAC) , KC_B    , KC_M    , KC_W    , KC_V        , KC_Z     , KC_RSFT  ,
+        KC_LCTL , KC_MEH      , KC_PC_CUT , KC_PC_COPY          , KC_PC_PASTE       ,                      KC_BSPC , KC_DEL  , KC_ENT  , KC_SPACE         ,           KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , KC_RCTRL
     ),
 
     [_MAC] = LAYOUT(
@@ -115,12 +114,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______ , _______ , _______ , _______ , _______ ,           _______ , _______ , _______ , _______ ,           KC_0    , KC_0       , KC_DOT         , KC_KP_ENTER , _______
     ),
 
-    [_RAISE] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-        KC_PSLS, KC_P7, KC_P8, KC_P9, KC_NLCK, _______, _______,                    _______, _______, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_F12,
-        KC_CAPS, KC_P4, KC_P5, KC_P6, KC_NLCK, _______, _______,                    _______, _______, _______, KC_P4, KC_P5, KC_P6, KC_NLCK,
-        _______, KC_P1, KC_P2, KC_P3, _______, _______, _______, _______,  _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, _______,
-        _______, KC_P0, KC_PDOT, KC_PENT, _______,    _______,   _______,  _______,    _______,    _______, KC_P0, KC_PDOT, KC_PENT, _______
+    [_SYMBOL] = LAYOUT(
+        _______ , _______ , _______ , _______ , _______ , _______ ,                                         _______ , _______ , _______  , _______ , _______   , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                     _______ , KC_PERC , KC_DLR  , KC_TILD  , KC_CIRC , KC_BSLASH , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                     _______ , KC_AMPR , KC_HASH , KC_GRAVE , KC_AT   , KC_PIPE   , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______   , _______ ,
+        _______ , _______ , _______ , _______ , _______ ,           _______ , _______ , _______ , _______ ,           _______ , _______  , _______ , _______   , _______
     ),
 
 };
@@ -140,12 +139,6 @@ static void print_status_narrow(void) {
     // Print current mode
     oled_write_P(PSTR("\n\n"), false);
     oled_write_ln_P(PSTR("MODE"), false);
-    oled_write_ln_P(PSTR(""), false);
-    if (keymap_config.swap_lctl_lgui) {
-        oled_write_ln_P(PSTR("MAC"), false);
-    } else {
-        oled_write_ln_P(PSTR("WIN"), false);
-    }
 
     switch (get_highest_layer(default_layer_state)) {
         case _BASE:
@@ -155,7 +148,7 @@ static void print_status_narrow(void) {
             oled_write_ln_P(PSTR("Mac"), false);
             break;
         default:
-            oled_write_P(PSTR("Undef"), false);
+            oled_write_ln_P(PSTR("Undef"), false);
     }
     oled_write_P(PSTR("\n\n"), false);
     // Print current layer
@@ -163,16 +156,13 @@ static void print_status_narrow(void) {
     switch (get_highest_layer(layer_state)) {
         case _MAC:
         case _BASE:
-            oled_write_P(PSTR("Base\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("Raise"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("Base"), false);
             break;
         case _NUMPAD:
-            oled_write_P(PSTR("Numpad"), false);
+            oled_write_ln_P(PSTR("Numpad"), false);
+            break;
+        case _SYMBOL:
+            oled_write_ln_P(PSTR("Symbol"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
@@ -210,20 +200,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_MAC:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_MAC);
-            }
-            return false;
-        case KC_LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-            } else {
-                layer_off(_LOWER);
-            }
-            return false;
-        case KC_RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-            } else {
-                layer_off(_RAISE);
             }
             return false;
         case KC_PRVWD:
